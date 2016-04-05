4c7205f1c2ac03d8c61ed985bf2ede92e4d7b4bf
Ricardo Tomasi <ricardobeat@gmail.com>
2016-04-05 13:47:43 +0200

    Use function shorthand

--- components.js
+++ components.js
@@ -13,50 +13,44 @@ function qs (selector, context = document) {
 
 // -----------------------------------------------------------------------------
 
-function initialize (Component, element) {
-    var instance = components[++uid] = new Component()
-    element._component = uid
-    return Object.assign(instance, {
-        el: element,
-        id: uid,
-    })
-}
-
-function load (element) {
-    var name = element.getAttribute('data-component')
-    if (!name) return
-
-    var Component = definitions[name]
-    if (!Component) throw new Error(`Component ${name} does not exist`)
-
-    render(initialize(Component, element))
-}
-
-function render (component) {
-    let rendered = `<div>${component.render().trim()}</div>`
-    morphdom(component.el, rendered, {
-        childrenOnly: true
-    })
-}
-
-function register (name, def) {
-    definitions[name] = def
-    return exports
-}
-
-function mount (root) {
-    if (typeof root === 'string') {
-        root = document.querySelector(root)
-    }
-    load(root)
-    qs('[data-component]', root).forEach(mount, this)
-}
-
 module.exports = {
-    definitions,
-    components,
-    initialize,
-    load,
-    register,
-    mount
+    
+    initialize (Component, element) {
+        var instance = components[++uid] = new Component()
+        element._component = uid
+        return Object.assign(instance, {
+            el: element,
+            id: uid,
+        })
+    }
+
+    load (element) {
+        var name = element.getAttribute('data-component')
+        if (!name) return
+
+        var Component = definitions[name]
+        if (!Component) throw new Error(`Component ${name} does not exist`)
+
+        render(initialize(Component, element))
+    }
+
+    render (component) {
+        let rendered = `<div>${component.render().trim()}</div>`
+        morphdom(component.el, rendered, {
+            childrenOnly: true
+        })
+    }
+
+    register (name, def) {
+        definitions[name] = def
+        return exports
+    }
+
+    mount (root) {
+        if (typeof root === 'string') {
+            root = document.querySelector(root)
+        }
+        load(root)
+        qs('[data-component]', root).forEach(mount, this)
+    }
 }
